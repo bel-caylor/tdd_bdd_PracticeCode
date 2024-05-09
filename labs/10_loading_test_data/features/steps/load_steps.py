e636e319380ae303d1131a722e9e9a029ef051b3
Belinda Caylor <belinda.caylor@gmail.com>
2024-05-09 10:53:12 -0400

    Add steps

--- labs/10_loading_test_data/features/steps/load_steps.py
+++ labs/10_loading_test_data/features/steps/load_steps.py
@@ -11,3 +11,25 @@ from behave import given
 
 # Load data here
 
+@given('the following pets')
+def step_impl(context):
+    """Refresh all Pets in the database"""
+
+    # List all pets and delete them one by one
+    response = requests.get(f"{context.base_url}/pets")
+    assert response.status_code == 200
+    for pet in response.json():
+        response = requests.delete(f"{context.base_url}/pets/{pet.id}")
+        assert response.status_code == 204
+
+    # Add New Pets from Background data
+    for pet in context.table:
+        payload = {
+            name: pet.name,
+            category: pet.category,
+            available: pet.available in ['True', 'true', '1'],
+            gender: pet.gender,
+            birthday: pet.birthday,
+        }
+        response = requests.push(f"{context.base_url}/pets", json=payload)
+        assert response.status_code == 201
